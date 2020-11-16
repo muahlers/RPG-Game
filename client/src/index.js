f421a52f30deaf57898465824025d5565172c8ef
muahlers <muahlers@gmail.com>
2020-11-16 00:54:25 -0300

    Webpack y Babel

---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import 'phaser';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 500,
+  scene: {
+      preload,
+      create
+  },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  console.log('this is the preload method');
+}
+
+function create() {
+  this.add.text(0, 0, 'Hello World 7');
+}
---
+++ client/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output:{
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'main-dev.js'
+  },
+  module:{
+    rules: [
+      {
+      test:[/\.vert$/, /\.frag$/],
+      use: 'raw-loader'
+    },
+      {
+      test: /\.js$/,
+      use: { loader: 'babel-loader'}
+    }
+  ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'CANVAS_RENDER': JSON.stringify(true),
+      'WEBGL_RENDER': JSON.stringify(true)
+    })
+  ]
+}
