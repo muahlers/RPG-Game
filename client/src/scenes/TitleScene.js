87f30fe19b86ccdd448d7de6979fd55f419a23bf
muahlers <muahlers@gmail.com>
2020-11-25 17:13:21 -0300

    css

--- client/src/scenes/TitleScene.js
+++ client/src/scenes/TitleScene.js
@@ -11,8 +11,6 @@ export default class TitleScene extends Phaser.Scene {
     this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 2, 'Zenva MMORPG', { fontSize: '64px', fill: '#fff' });
     this.titleText.setOrigin(0.5);
 
-    // create the Play game button
-    // this.startGameButton = new UiButton(this, this.scale.width / 2, this.scale.height * 0.65, 'button1', 'button2', 'Start', this.startScene.bind(this, 'Game'));
     // create the Login game button
     this.startGameButton = new UiButton(
       this, this.scale.width / 2,
